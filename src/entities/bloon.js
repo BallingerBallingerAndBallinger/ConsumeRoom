2dbb16e0cc10450cb14a69aeb9b7999017711958
Andrew Ballinger <andrewballinger@stratisopt.com>
2016-12-10 13:24:03 -0800

    Drifting bloon

--- src/entities/bloon.js
+++ src/entities/bloon.js
@@ -2,28 +2,36 @@
   var entityBase = require('../crappy-entity.js');
   var sprites = require('../crappy-sprites.js');
 
+  var velocity = 0.01;
+
   function initialize(canvasElement, moveMethod, checkMovement) {
     var constructor = () => {
-      var entity = entityBase.initialize(canvasElement,
-                                         moveMethod);
-      sprites.initialize(canvasElement);
-
-      var goingLeft = false;
-
+      var entity = entityBase.initialize(canvasElement, moveMethod);
       var self = { name: 'bloon', x: 100, y: 400, size: 100 };
       var bloon = Object.assign({}, entity);
+      var goingLeft = false;
+      var travel = 0;
+
+      sprites.initialize(canvasElement);
 
       bloon.update = update;
       return bloon;
 
       function update(timestamp, delta) {
-        var attemptedX = goingLeft ? self.x - 10 : self.x + 10;
+        var attemptedTravel = goingLeft ? -1 * velocity * delta : velocity * delta;
+        var attemptedX = attemptedTravel + self.x;
         var toMove = checkMovement(attemptedX, self.y);
+
+        if (travel > 10 || travel < -10) {
+          goingLeft = !goingLeft;
+          travel = 0;
+        }
 
         if (toMove === false) {
           goingLeft = !goingLeft;
         } else {
           self.x = attemptedX;
+          travel += attemptedTravel;
         };
 
         sprites.update([self]);
