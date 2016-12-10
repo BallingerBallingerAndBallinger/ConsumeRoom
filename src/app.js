15092239a4b871bc172eb036a3513c8178fd852f
Nic Ballinger <nic@Nics-MBP.fios-router.home>
2016-12-09 22:37:45 -0600

    Test update to insult Andrew

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-(function(){
+(function() {
   'use strict';
 
   var config = require('./configuration.js');
 
   window.onload = () => {
     var canvas = document.getElementById('canvas').getContext('2d');
-    canvas.fillText('Hello Ludem Dare!', 10, 50);
+    canvas.fillText('Hello Ludem Andrew is insulted!', 10, 50);
     canvas.fillText(config.title, 10, 90);
   };
 })();
