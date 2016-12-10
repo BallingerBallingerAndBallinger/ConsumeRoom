c1c0f785a39243056d12d6ac9faa4393c2a571f3
Nic Ballinger <nic@Nics-MBP.fios-router.home>
2016-12-09 22:21:45 -0600

    WOOT!

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-(function(){
+(function() {
   'use strict';
 
   window.onload = () => {
     var canvas = document.getElementById('canvas').getContext('2d');
     canvas.fillText('Hello World', 10, 50);
   };
-
 })();
