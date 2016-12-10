324951ef87d0fcf72a0d149b2df9613074474c52
Andrew Ballinger <andrewballinger@stratisopt.com>
2016-12-09 21:57:33 -0800

    He's dancing!

--- docs/app.js
+++ docs/app.js
@@ -44,26 +44,53 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
+	/* globals requestAnimationFrame */
 	(function() {
 	  'use strict';
 
 	  var config = __webpack_require__(1);
 	  var sprites = __webpack_require__(2);
 
+	  var canvasElement;
 	  window.onload = () => {
-	    var canvasElement = document.getElementById('canvas');
-
+	    canvasElement = document.getElementById('canvas');
 	    sprites.initialize(canvasElement);
+	    requestAnimationFrame(grandLoop);
+	  };
+
+	  function grandLoop(timestamp) {
+	    drawFrame(timestamp);
+	    requestAnimationFrame(grandLoop);
+	  }
+
+	  function drawFrame(timestamp) {
+	    var delta = getDelta(timestamp);
+	    if (delta < 50) return;
+	    updateDelta(timestamp);
+
 	    sprites.update([
 	      { name: 'crappy-room', x: 25, y: 10, size: 100 },
-	      { name: 'crappy-party-dude', x: 50, y: 45, size: 50 },
+	      { name: 'crappy-party-dude', x: Math.random() * 50, y: 45, size: 50 },
 	      { name: 'crappy-party-dude', x: 90, y: 30, size: 40 },
 	      { name: 'crappy-party-dude', x: 25, y: 10, size: 30 }
 	    ]);
 	    sprites.draw();
-
 	    drawTitle(canvasElement.getContext('2d'));
-	  };
+	  }
+
+	  var last;
+	  function getDelta(timestamp) {
+	    if (last === undefined) {
+	      last = timestamp;
+	      return 0;
+	    }
+
+	    return timestamp - last;
+	  }
+
+	  function updateDelta(timestamp) {
+	    last = timestamp;
+	  }
 
 	  // Taken from a horrific w3c example
 	  function drawTitle(ctx) {
--- src/app.js
+++ src/app.js
@@ -1,23 +1,50 @@
+/* globals requestAnimationFrame */
 (function() {
   'use strict';
 
   var config = require('./configuration.js');
   var sprites = require('./crappy-sprites.js');
 
+  var canvasElement;
   window.onload = () => {
-    var canvasElement = document.getElementById('canvas');
-
+    canvasElement = document.getElementById('canvas');
     sprites.initialize(canvasElement);
+    requestAnimationFrame(grandLoop);
+  };
+
+  function grandLoop(timestamp) {
+    drawFrame(timestamp);
+    requestAnimationFrame(grandLoop);
+  }
+
+  function drawFrame(timestamp) {
+    var delta = getDelta(timestamp);
+    if (delta < 50) return;
+    updateDelta(timestamp);
+
     sprites.update([
       { name: 'crappy-room', x: 25, y: 10, size: 100 },
-      { name: 'crappy-party-dude', x: 50, y: 45, size: 50 },
+      { name: 'crappy-party-dude', x: Math.random() * 50, y: 45, size: 50 },
       { name: 'crappy-party-dude', x: 90, y: 30, size: 40 },
       { name: 'crappy-party-dude', x: 25, y: 10, size: 30 }
     ]);
     sprites.draw();
-
     drawTitle(canvasElement.getContext('2d'));
-  };
+  }
+
+  var last;
+  function getDelta(timestamp) {
+    if (last === undefined) {
+      last = timestamp;
+      return 0;
+    }
+
+    return timestamp - last;
+  }
+
+  function updateDelta(timestamp) {
+    last = timestamp;
+  }
 
   // Taken from a horrific w3c example
   function drawTitle(ctx) {
