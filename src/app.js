9523e8b48bfb7c2f337afcc8b3b8885e39c68516
Nic Ballinger <nic@nicballinger.com>
2016-12-09 23:43:08 -0600

    Party Dudegit status

--- src/app.js
+++ src/app.js
@@ -7,7 +7,10 @@
   window.onload = () => {
     sprites.initialize(document.getElementById('canvas'));
     sprites.update([
-      { name: 'crappy-room', x: 25, y: 10, size: 100}
+      { name: 'crappy-room', x: 25, y: 10, size: 100 },
+      { name: 'crappy-party-dude', x: 50, y: 45, size: 50 },
+      { name: 'crappy-party-dude', x: 90, y: 30, size: 40 },
+      { name: 'crappy-party-dude', x: 25, y: 10, size: 30 }
     ]);
     sprites.draw();
   };
