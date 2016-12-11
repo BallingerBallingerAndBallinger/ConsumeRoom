16c8a37906521d989f79886e85470e222508d4ac
DanaBallinger <danaballinger@Danas-MBP.fios-router.home>
2016-12-10 18:39:33 -0600

    Pathing

--- src/crappy-entity.js
+++ src/crappy-entity.js
@@ -4,8 +4,9 @@
 
   function initialize(renderer, moveMethod) {
     var initializer = () => {
-      var self = {name: 'generic', x: 1, y: 2};
+      var self = {name: 'generic', x: 1, y: 2, vx: 0, vy: 0, gx: 0, gy: 0};
       var render = renderer;
+      var steps;
       return {
         update: update
       };
@@ -15,6 +16,43 @@
         render.circle(self.x, self.y, 20, 'black', 'red');
         console.log("We're totally rendering an entity right now");
       }
+
+      function setGoal() {
+
+        // Set distance
+        var distance = 10 + Math.Random() * 100;
+
+        // Set a goal
+        var angle = Math.Random() * Math.PI * 2;
+        self.gx = Math.sin(angle) * distance;
+        self.gy = Math.cos(angle) * distance;
+
+        // Set steps
+        steps = 2 + Math.Random() * 100;
+
+        // Set speed
+        var speed = distance / steps;
+        self.vx = (self.x - self.gx) * speed;
+        self.vy = (self.y - self.gy) * speed;
+
+      }
+
+      function moveTowardGoal() {
+
+        if (steps == 0) setGoal();
+
+        var newx = self.x+self.vx;
+        var newy = self.y+self.vy;
+        var toMove = checkMovement(newx, newy);
+        if (toMove == true) {
+          self.x = newx;
+          self.y = newy;
+          steps--;
+        }
+
+      }
+
+
     };
     return initializer();
   }
