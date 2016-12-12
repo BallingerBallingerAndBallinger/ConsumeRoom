7b2d6a47878e8a9c7cec2654a240996f2a5c0ae0
Andrew Ballinger <andrewballinger@stratisopt.com>
2016-12-11 19:18:32 -0800

    build

--- docs/app.js
+++ docs/app.js
@@ -189,7 +189,7 @@
 	        introducePartygoer();
 	      }
 	
-	      if (Math.random() < config.basePartyGoerLeavesProbability) {
+	      if (party.rollLeaver()) {
 	        partyGoerWantsToLeave();
 	      }
 	    }
@@ -17741,7 +17741,18 @@
 	    var roll = Math.random();
 	    var irresistibility = (gameState.getEnticingness() / config.irresistableEnticingness);
 	    var required = config.basePartyGoerProbability + irresistibility;
-	    console.log(roll + " out of " + required);
+	    if (roll < required) {
+	      return true;
+	    }
+	  }
+	
+	  function rollLeaver() {
+	    var roll = Math.random();
+	
+	    var people = gameState.getPeopleCount();
+	    var packedPenalty = (people / config.packedHouse);
+	    var required = config.basePartyLeavesProbability + packedPenalty;
+	 
 	    if (roll < required) {
 	      return true;
 	    }
@@ -17760,7 +17771,8 @@
 	
 	  module.exports = {
 	    getPartyGoer: getPartyGoer,
-	    rollGoer: rollGoer
+	    rollGoer: rollGoer,
+	    rollLeaver: rollGoer
 	  };
 	})();
 
