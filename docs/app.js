845a92c9f7af9ea6abcddf78fa0c1bcbe32af2af
Nic Ballinger <nic@nicballinger.com>
2016-12-11 22:37:27 -0600

    fixed class add bug

--- docs/app.js
+++ docs/app.js
@@ -18670,15 +18670,13 @@
 
 	(() => {
 	  function addClass(element, klass) {
-	    var classes = element.className.match(/(^| ).+?($| )/g);
-	    classes = classes.map(s => s.trim());
+	    var classes = element.className.split(' ');
 	    if (classes.includes(klass)) return;
 	    element.className += ' ' + klass;
 	  }
 	
 	  function removeClass(element, klass) {
-	    var classes = element.className.match(/(^| ).+?($| )/g);
-	    classes = classes.map(s => s.trim());
+	    var classes = element.className.split(' ');
 	    element.className = classes.filter((k) => k !== klass).join(' ');
 	  }
 	
